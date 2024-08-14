2f426b2db1cf80d2c44043455bd31cd86d8d9cdf
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-08-15 00:01:33 +0300

    setup them configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,36 +4,38 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
       },
       colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
+        primary: "var(--text-primary)",
+        secondary: "var(--text-secondary)",
+      },
+      backgroundColor: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        card: "var(--card)",
+        background: "var(--background)",
+      },
+      borderColor:{
+        primary: 'var(--border-color)'
       },
       fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
       },
-      animation:{'spin-slow': 'spin 3s linear infinite',},
+      animation: { "spin-slow": "spin 3s linear infinite" },
       extend: {
         spacing: {
-          '128': '32rem',
-          '144': '36rem',
+          128: "32rem",
+          144: "36rem",
         },
         borderRadius: {
-          '4xl': '2rem',
-        }
-      }
+          "4xl": "2rem",
+        },
+      },
     },
   },
   plugins: [],
