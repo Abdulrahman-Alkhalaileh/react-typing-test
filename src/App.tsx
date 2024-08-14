66c3b01fc1e760fee95f6c137d47e674e8416e08
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-08-14 22:16:06 +0300

    setup react with typescript & tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-3xl p-2 flex bg-pink justify-center gap-4 h-lvh items-center">
+      <p className='hover:animate-spin-slow md:text-blue lg:text-white xl:text-black'>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default App;
